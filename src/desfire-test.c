981e5a4ad2ec5e97312df9c855e9769b0073ee93
Raphael Esterle <hansahanz@gmail.com>
2016-03-06 23:40:36 +0100

    better error handling

--- src/desfire-test.c
+++ src/desfire-test.c
@@ -14,9 +14,20 @@ int init_nfc(nfc_context** context) {
 		errx(EXIT_FAILURE, "Unable to init context");
 }
 
+void cleanup(nfc_context *context, nfc_device *device, FreefareTag *tag){
+    if(tag)
+	    mifare_desfire_disconnect(*tag);
+    if(device)
+	    nfc_close(device);
+    nfc_exit (context);
+}
 
+void finalize_error(FreefareTag* tag, nfc_device *device, nfc_context* context, char message[]){
+    cleanup(context, device, tag);
+	err(EXIT_FAILURE, message);
+}
 
-int get_tag(nfc_context* context, FreefareTag* tag){
+void get_tag(nfc_context* context, FreefareTag* tag){
 	
     nfc_connstring devices[8];
     size_t device_count;
@@ -26,9 +37,9 @@ int get_tag(nfc_context* context, FreefareTag* tag){
 	// List devices
     device_count = nfc_list_devices (context, devices, 8);
     if (device_count <= 0)
-		err(EXIT_FAILURE, "No NFC device found.");
+		errx(EXIT_FAILURE, "No NFC device found.");
 	if (device_count > 1)
-		err(EXIT_FAILURE, "Only one connected device is sopported");
+		errx(EXIT_FAILURE, "Only one connected device is sopported");
 
 	// Open device
 	device = nfc_open(context, devices[0]);
@@ -38,34 +49,34 @@ int get_tag(nfc_context* context, FreefareTag* tag){
 
 	// Try to get tag 
 	tags = freefare_get_tags (device);
-	if (!tags) {
-		nfc_close (device);
-		errx (EXIT_FAILURE, "Error listing tags.");
-	}
+	if (!tags)
+        finalize_error(NULL, device, context, "Error listing tags.");
 
 	if(!tags[0])
-		errx(EXIT_FAILURE, "No tag detected");
-	if(tags[1])
-		errx (EXIT_FAILURE, "More than one tag detected");
+        finalize_error(NULL, device, context, "No tag detected.");
+	if(tags[1]){
+        finalize_error(NULL, device, context, "More than one tag detected");
+    }
 
 	*tag = tags[0];
+    // Check if tag is a desfire tag
 	if (MIFARE_DESFIRE != freefare_get_tag_type (*tag))
-		errx (EXIT_FAILURE, "Tag is not a desfire tag");
+        finalize_error(NULL, device, context, "Tag is not a desfire tag");
 
 	// Try to connect to tag
 	if(mifare_desfire_connect(*tag) != 0)
-		errx (EXIT_FAILURE, "Cannot connect to tag");
+        finalize_error(NULL, device, context, "Cannot connect to tag");
 }
 
 int get_default_key(FreefareTag tag, uint8_t key_no){
 
-	char *tag_uid = freefare_get_tag_uid (tag);
-
 	// Try to authenticate with a default key and store it
 	if(mifare_desfire_auto_authenticate(tag, key_no) != 0){
-		printf("Cannot get dafault key for uid: %s\n", tag_uid);
-		exit(1);
+        return 1;
+        // TODO error handling 
+		printf("Cannot get dafault key for uid: \n");
 	} 
+    return 0;
 }
 
 int get_application_ids(FreefareTag tag, MifareDESFireAID** aids, size_t* count){
@@ -101,10 +112,10 @@ int main(int argc, char *argv[]) {
 
 	printf("found %d applicaton/s\n", (int)count);
 
-	int j;
-	for(j=0; j<count; j++){
-		aid = aids[j];
-		printf("aid %d: %x\n", j, mifare_desfire_aid_get_aid(aid));
+	int i,j;
+	for(i=0; i<count; i++){
+		aid = aids[i];
+		printf("aid %d: %x\n", i, mifare_desfire_aid_get_aid(aid));
 
 		size_t count2;
 		uint8_t *file_ids = malloc(sizeof(uint8_t));
@@ -114,11 +125,10 @@ int main(int argc, char *argv[]) {
 			continue;
 		printf("found %d file ids\n", (int)count2);
 
-		int i;
-		for(i=0; i<count2; i++){
-			printf("	File id: %x ", file_ids[i]);
+		for(j=0; j<count2; j++){
+			printf("	File id: %x ", file_ids[j]);
 			struct mifare_desfire_file_settings settings;
-			mifare_desfire_get_file_settings (tag, file_ids[i], &settings);
+			mifare_desfire_get_file_settings (tag, file_ids[j], &settings);
 			printf("  Type: %x", settings.file_type);
 			printf("  com settings: %x", settings.communication_settings);
 			printf("  standard f size: %x", settings.settings.standard_file.file_size); 
@@ -126,10 +136,5 @@ int main(int argc, char *argv[]) {
 		}
 
 	}
-			
-
-	mifare_desfire_disconnect(tag);
-	//nfc_close (device);
-    nfc_exit (context);
-} /* main() */
-
+    return 0;
+}
